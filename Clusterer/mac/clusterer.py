af06dff6ac3f641abfffbd7e63f1c4a212ee1d78
David Caratelli <dcaratelli@houston.nevis.columbia.edu>
2015-09-21 12:13:08 -0400

    added simple script -- David Caratelli

---
+++ Clusterer/mac/clusterer.py
@@ -0,0 +1,51 @@
+import sys
+
+if len(sys.argv) < 2:
+    msg  = '\n'
+    msg += "Usage 1: %s $INPUT_ROOT_FILE\n" % sys.argv[0]
+    msg += '\n'
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+
+from ROOT import gSystem,TMath
+from ROOT import larlite as fmwk
+from ROOT import larutil
+
+# Create ana_processor instance
+my_proc = fmwk.ana_processor()
+
+# Set input root file
+for x in xrange(len(sys.argv)-1):
+    fname = sys.argv[x+1]
+    my_proc.add_input_file(fname)
+    
+# Specify IO mode
+my_proc.set_io_mode(fmwk.storage_manager.kBOTH)
+
+# Specify analysis output root file name
+my_proc.set_ana_output_file("");
+
+# Specify data output root file name
+my_proc.set_output_file("larlite_rawclusters.root")
+
+clusterer = fmwk.SimpleClusterer()
+#clusterer.setHitProducer('rawhit')
+#clusterer.setHitProducer('cchit')
+clusterer.setHitProducer('gausshit')
+clusterer.setVerbose(True)
+
+my_proc.add_process(clusterer)
+
+my_proc.set_data_to_write(fmwk.data.kHit,'gausshit')
+my_proc.set_data_to_write(fmwk.data.kCluster,'rawcluster')
+my_proc.set_data_to_write(fmwk.data.kAssociation,'rawcluster')
+
+print
+print  "Finished configuring ana_processor. Start event loop!"
+print
+
+my_proc.run()
+
+sys.exit()
+
